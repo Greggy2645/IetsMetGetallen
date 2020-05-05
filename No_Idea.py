6ef2fda3581a4b2206d3e1bde6bcbce327d4d841
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-05 12:45:49 +0200

    Update No_Idea.py
    I've added testscriptje() to test my ideas

--- No_Idea.py
+++ No_Idea.py
@@ -5,20 +5,23 @@ print(alle_getallen)
 print('dit was een testje, next \n')
 
 def startding():
-    keuze = str(input('kies a voor printen, enige optie mogenlijk lol'))
+    keuze = str(input('kies a voor printen, enige optie mogenlijk lol\n'))
     if keuze == 'a':
         printen()
-    #elif keuze == 'b':
-        #not-defined-yet()
-#what I'm going to try is alle_getallen+1 but that wont work somehow.
+    elif keuze == 'b':
+        testscriptje()
+
 def printen():
 	alle_getallen=list(range(0, int(input("tot welk getal? \n"))))
-  #alle_getallen = int(alle_getallen)
-  #alle_getallen = alle_getallen + 1
-	#WAIT I KNOW HOW THIS WORKS
+	alle_getallen = int(alle_getallen)
 	print("printingg")
 	for i in alle_getallen:
 		print(str(i))
 		time.sleep(0.1)
 	printen()
 startding()
+
+def testscriptje():
+	getal = int(input("kies getal"))
+	range_ding = list(range(0, getal))
+	print(range_ding)
