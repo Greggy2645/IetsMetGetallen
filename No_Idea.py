d8424ca6052045adc5e192a57a079bd0f92859f7
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-05 12:40:12 +0200

    Create No_Idea.py

---
+++ No_Idea.py
@@ -0,0 +1,23 @@
+#alles bij elkaar doen 123
+import time
+alle_getallen = list(range(1, 5))
+print(alle_getallen)
+print('dit was een testje, next \n')
+
+def startding():
+    keuze = str(input('kies a voor printen, enige optie mogenlijk lol'))
+    if keuze == 'a':
+        printen()
+    #elif keuze == 'b':
+        #not-defined-yet()
+#what I'm going to try is alle_getallen+1 but that wont work somehow.
+def printen():
+	alle_getallen=list(range(0, int(input("tot welk getal? \n"))))
+  #alle_getallen = int(alle_getallen)
+  #alle_getallen = alle_getallen + 1
+	print("printingg")
+	for i in alle_getallen:
+		print(str(i))
+		time.sleep(0.1)
+	printen()
+startding()
